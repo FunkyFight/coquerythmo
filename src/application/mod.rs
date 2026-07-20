c469d3bfd30436c4f1b58019c2ced6f62dae461c
FunkyFight <45899449+FunkyFight@users.noreply.github.com>
2026-07-20 18:27:52 +0200

    Expose detection edit service

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -4,6 +4,7 @@ pub mod collaboration_service;
 pub mod command;
 pub mod context;
 pub mod delta_codec;
+pub mod detection_service;
 pub mod edit_service;
 pub mod job_service;
 pub mod playback_service;
